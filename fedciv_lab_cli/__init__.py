dd94ae31b5a8b44f17021dec4ebc0ea02bba90f6
sambyers <samwbyers@gmail.com>
2022-03-31 15:00:39 -0400

    formatting and adding example command to api

--- fedciv_lab_cli/__init__.py
+++ fedciv_lab_cli/__init__.py
@@ -9,42 +9,46 @@ from requests.exceptions import HTTPError
 def cli():
     pass
 
+
 @click.command()
 @click.option("--arg1", help="Input arg1")
-@click.option("--arg2", help ="Input arg2")
+@click.option("--arg2", help="Input arg2")
 def reset_lab(arg1, arg2):
-    #send info to localhost:5000 running the flask app/API backend
+    # send info to localhost:5000 running the flask app/API backend
     try:
-        #template api call for now
-        requests.post('127.0.0.1:5000/reset', data = {"devices":'device'})
-        
-    except requests.exceptions.RequestException as e:  
+        # template api call for now
+        requests.post("127.0.0.1:5000/reset", data={"devices": "device"})
+
+    except requests.exceptions.RequestException as e:
         raise SystemExit(e)
-    
-    print('you inputted arg1: ', arg1, ' arg2 is: ', arg2)
+
+    print("you inputted arg1: ", arg1, " arg2 is: ", arg2)
+
 
 @click.command()
 @click.option("--arg1", help="input arg1")
 def lab_status(arg1):
-    #send info to localhost:5000 running the flask app/API backend
+    # send info to localhost:5000 running the flask app/API backend
     try:
-        #template api call for now
-        resp = requests.get('http://127.0.0.1:5000/status')
-        
-    except requests.exceptions.RequestException as e:  
+        # template api call for now
+        resp = requests.get("http://127.0.0.1:5000/status")
+
+    except HTTPError as e:
         raise SystemExit(e)
-    print('you inputted arg1: ', arg1)
+    print("you inputted arg1: ", arg1)
     print(json.dumps(resp.json(), indent=4))
+
 
 @click.command()
 def example():
     # Example of request to API that will call to external service
     try:
-        resp = requests.get('http://127.0.0.1:5000/example')
-    except requests.exceptions.RequestException as e:
+        resp = requests.get("http://127.0.0.1:5000/example")
+    except HTTPError as e:
         raise SystemExit(e)
     print("This is an example command that calls the API for an external service.")
     print(json.dumps(resp.json(), indent=4))
+
 
 cli.add_command(reset_lab)
 cli.add_command(lab_status)
@@ -52,13 +56,3 @@ cli.add_command(example)
 
 if __name__ == "__main__":
     cli()
-
-    
-
-
-
-   
-
-
-
-
--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='fedciv_lab_cli',
-    version='0.1.0',
+    name="fedciv_lab_cli",
+    version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Click',
-        'requests',
+        "Click",
+        "requests",
     ],
     entry_points={
-        'console_scripts': [
-            'civlab = fedciv_lab_cli:cli',
+        "console_scripts": [
+            "civlab = fedciv_lab_cli:cli",
         ],
     },
 )
