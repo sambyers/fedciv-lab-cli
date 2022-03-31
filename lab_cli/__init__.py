5e545e5e6f2269a1973e5793e346ff0d3247e06e
Yashar Asgari <yasharasgari97@gmail.com>
2022-03-31 11:02:40 -0400

    add template code for requests call

--- lab_cli/__init__.py
+++ lab_cli/__init__.py
@@ -1,6 +1,9 @@
 # Main CLI code here
 
 import click
+import requests
+from requests.exceptions import HTTPError
+
 
 @click.group()
 def cli():
@@ -10,13 +13,26 @@ def cli():
 @click.option("--arg1", help="Input arg1")
 @click.option("--arg2", help ="Input arg2")
 def reset_lab(arg1, arg2):
-    #do something
+    #send info to localhost:5000 running the flask app/API backend
+    try:
+        #template api call for now
+        requests.post('127.0.0.1:5000/reset', data = {"devices":'device'})
+        
+    except requests.exceptions.RequestException as e:  
+        raise SystemExit(e)
+    
     print('you inputted arg1: ', arg1, ' arg2 is: ', arg2)
 
 @click.command()
 @click.option("--arg1", help="input arg1")
 def lab_status(arg1):
-    #do something
+    #send info to localhost:5000 running the flask app/API backend
+    try:
+        #template api call for now
+        requests.post('127.0.0.1:5000/reset', data = {"devices":'device'})
+        
+    except requests.exceptions.RequestException as e:  
+        raise SystemExit(e)
     print('you inputted arg1: ', arg1)
 
 
