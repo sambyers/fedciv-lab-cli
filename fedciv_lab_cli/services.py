13796eb492c6a0973427a88c59e413f1b4d41ea9
Yashar Asgari <yasharasgari97@gmail.com>
2022-04-12 15:36:39 -0400

    build out reset functions in services

--- fedciv_lab_cli/services.py
+++ fedciv_lab_cli/services.py
@@ -16,7 +16,30 @@ class LabAPI:
         return resp.json()
 
     def start_reset(self, name: str = None):
-        r = {"status": "Not implemented"}
-        if name:
-            r.update({"name": name})
-        return r
+        try:
+            if 'dnac' in name:
+                resp = requests.put(f"{self.base_url}/reset/dnac")
+            elif 'ise' in name:
+                resp = requests.put(f"{self.base_url}/reset/ise")
+            elif 'vmanage' in name:
+                resp = requests.put(f"{self.base_url}/reset/vmanage")
+
+                #pass name which would be hostname for a specific device
+            elif len(name) > 1:
+                resp = requests.put(f"{self.base_url}/reset/{name}")
+
+                #if name var is 0, reset whole lab
+            else:
+                resp = requests.put(f"{self.base_url}/reset")
+        except requests.exceptions.HTTPError as e:
+            raise SystemExit(e)
+        
+        return resp.json()
+
+    def job_status(self, jobID: str = None):
+        try:
+            resp = requests.put(f"{self.base_url}/job/{jobID}")
+        except requests.exceptions.HTTPError as e:
+            raise SystemExit(e)
+
+        return resp.json()
